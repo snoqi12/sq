c8055e4d904c52452e842604f7bc93aa5e29e7ba
Rezky Aligrah <101689234+Rexaligra@users.noreply.github.com>
2025-06-09 17:38:34 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ export default function App() {
   const fetchPrediction = async () => {
     setLoading(true);
     try {
-      const res = await fetch(`http://localhost:5050/predict?coin=${coin}`);
+      const res = await fetch(`https://cryptoui-production.up.railway.app/predict?coin=${coin}`);
       const json = await res.json();
       setData(json);
     } catch (err) {
